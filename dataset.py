e2759419083b55a64541d7b0dd07f6ccaf8bbf7c
PeterArgueta <peterarguedo@gmail.com>
2023-03-01 18:10:09 -0600

    modified:   dataset.py

--- dataset.py
+++ dataset.py
@@ -16,40 +16,30 @@ df['FECHA']=pd.to_datetime(df['FECHA'], format="%d/%m/%Y")
 df = df.sort_values("FECHA")
 
 
-#agrupa pero sólo uno por cada fecha por lo tanto no funciona
-
-""" grouped = df.groupby("FECHA")
-first_values = grouped.first()
- """
-
- #print(first_values)
-
-
-
 ID={"CHAMPERICO FEGUA":"INS110701CV","COBAN":"INS160101CV","ESQUIPULAS":"INS200701CV","FLORES AEROPUERTO":"INS170101CV",\
     "LA AURORA":"INS010102CV"}
-        
+
 for k in ID:
     
     output_file(''+directory+''+k+'.html')
     
     gk=df.groupby(['VARIABLE'])
-    data=gk.get_group('LLUVIA')
+    precip=gk.get_group('LLUVIA')
     temp=gk.get_group('TMED')
     tempmin=gk.get_group('TMIN')
     tempmax=gk.get_group('TMAX')
 
-    variable=data[k]
+    precipitacion=precip[k]
     temperatura=temp[k]
     temperaturamin=tempmin[k]
     temperaturamax=tempmax[k]
-    fecha=data['FECHA']
+    fecha=precip['FECHA']
     
     TOOLS = "save,pan,box_zoom,reset,wheel_zoom"
     fig = figure(x_axis_type='datetime', title=k,
                  plot_height=400, plot_width=900, toolbar_location='below',
                  y_axis_label="Valor", y_range=(-5, 90), background_fill_color='white', background_fill_alpha=0.6, tools=TOOLS)
-    line = fig.line(fecha,variable,
+    line = fig.line(fecha,precipitacion,
                     line_color="deepskyblue", line_width=1, legend_label='Precipitación (mm)')
     line = fig.line(fecha,temperatura,
                     line_color="green", line_width=1, legend_label='Temperatura C')
@@ -66,4 +56,3 @@ for k in ID:
     p = gridplot([[fig]])
 
     show(p)
- 
